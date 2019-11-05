b88dbced40ce10b2f7c2f49cdc7155ad991e09fd
Andrew Lin <andrewlin618@gmail.com>
2019-11-05 11:02:49 -0800

    debug

--- controllers/shiftsControllers.js
+++ controllers/shiftsControllers.js
@@ -1,6 +1,5 @@
 const db = require("../models");
 
-// Defining methods for the booksController
 module.exports = {
     findAll: function(req, res) {
     db.Shifts
@@ -33,5 +32,5 @@ module.exports = {
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
-  }
+    }
 };
